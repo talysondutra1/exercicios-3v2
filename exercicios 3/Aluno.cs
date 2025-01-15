b8dc5948903ee287c18a4430171f62db04bdefdf
Talyson Dutra <talysondutrademacena@gmail.com>
2025-01-14 23:40:58 -0300

    Adicionar arquivos de projeto.

---
+++ exercicios 3/Aluno.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Xml;
+
+namespace exercicios_3
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double N1, N2, N3;
+        public double Resultado;
+        public double NotaFinal()
+        {
+            return N1 + N2 + N3;
+        }
+        public bool Aprovado()
+        {
+            if (NotaFinal() >= 60)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double NotaRestante()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - NotaFinal();
+            }
+        }
+    }
+}
+
+
---
+++ exercicios 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using exercicios_3;
+using System;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a1 = new Aluno();
+
+            Console.WriteLine("Digite o nome do aluno: ");
+            a1.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas que o aluno tirou: ");
+            a1.N1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a1.N2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a1.N3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL: " + a1.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (a1.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("Faltaram: " + a1.NotaRestante().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
